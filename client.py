7299d54ebdd2785793bc2cf7550381689532dc3b
schmutov <chmutovsergey@gmail.com>
2023-10-11 20:34:11 +0300

    Добавить клиентскую часть

--- client.py
+++ client.py
@@ -1,9 +1,76 @@
-# import asyncio
+import asyncio
+import json
 
 
 class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+    def __init__(
+            self,
+            user_id,
+            chat_room_id,
+            server_host="127.0.0.1",
+            server_port=8888
+    ):
+        self.user_id = user_id
+        self.chat_room_id = chat_room_id
+        self.server_host = server_host
+        self.server_port = server_port
 
-    def send(self, message=""):
-        pass
+    async def connect(self):
+        self.reader, self.writer = await asyncio.open_connection(self.server_host, self.server_port)
+
+        # while True:
+        for i in range(100):
+            await self.send(self.reader, self.writer, i)
+
+        print("Close the connection")
+        self.writer.close()
+
+    async def send(self, reader, writer, i):
+        message = json.dumps(
+            {
+                "message": f"Сообщение {i} от {self.user_id}",
+                "chat_room_id": self.chat_room_id,
+                "author_id": self.user_id,
+            }
+        ).encode()
+        self.reliable_send(data=message)
+
+        await asyncio.sleep(5)
+        await self.writer.drain()
+
+    def reliable_send(self, data: bytes) -> None:
+        """
+        Функция отправки данных в сокет
+        Обратите внимание, что данные ожидаются сразу типа bytes
+        """
+        # Разбиваем передаваемые данные на куски максимальной длины 0xffff (65535)
+        for chunk in (data[_:_ + 0x1f] for _ in range(0, len(data), 0x1f)):
+            self.writer.write(len(chunk).to_bytes(2, "big"))
+            self.writer.write(chunk)
+
+        self.writer.write(len("").to_bytes(2, "big"))
+
+
+async def tcp_echo_client1(message):
+    message = f"{message} Клиент 1"
+    reader, writer = await asyncio.open_connection(
+        "127.0.0.1", 8888)
+
+    while True:
+        await asyncio.sleep(5)
+        print(f"Send: {message!r}")
+        writer.write(message.encode())
+        await writer.drain()
+
+        data = await reader.read(100)
+        print(f"Received: {data.decode()!r}")
+
+    print("Close the connection")
+    writer.close()
+
+client = Client(
+    user_id="81c95c92-8a18-4b22-8ffe-b4163443162f",
+    chat_room_id="9b8466d0-df95-4ec9-b9a3-b011577bf046"
+)
+
+asyncio.run(client.connect())
--- server.py
+++ server.py
@@ -1,27 +1,46 @@
 import asyncio
+import json
 
 from config.logger import logger
+from config.session import async_session
+from model import MessageModel
 
 
 class Server:
     def __init__(self, host="127.0.0.1", port=8888):
         self.host = host
         self.port = port
+        self.loop = asyncio.get_event_loop()
         pass
 
     async def listen(self):
         pass
 
+    async def reliable_receive(self) -> bytes:
+        """
+        Функция приёма данных
+        Обратите внимание, что возвращает тип bytes
+        """
+        b = b''
+        while True:
+            part_len = int.from_bytes(await self.reader.readexactly(2),
+                                      "big")  # Определяем длину ожидаемого куска
+            if part_len == 0:  # Если пришёл кусок нулевой длины, то приём окончен
+                return b
+            b += await self.reader.readexactly(part_len)  # Считываем сам кусок
+
     async def handle_echo(self, reader, writer):
-        data = await reader.read(100)
-        message = data.decode()
-        addr = writer.get_extra_info("peername")
+        self.reader = reader
+        self.writer = writer
 
-        print(f"Received {message!r} from {addr!r}")
+        while True:
+            f = self.loop.run_until_complete(self.reliable_receive())
+            print(f)
 
-        print(f"Send: {message!r}")
-        writer.write(data)
-        await writer.drain()
+            addr = writer.get_extra_info('peername')
+            print(addr)
+
+            await writer.drain()
 
         print("Close the connection")
         writer.close()
